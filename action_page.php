62f9fb81985096cae827e729149ca7b6aa33114f
ttl2132 <32350953+ttl2132@users.noreply.github.com>
2017-12-02 20:48:28 -0500

    Website

---
+++ action_page.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ViTech Insurance Analytics</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/scrolling-nav.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="vitech.css">
+</head>
+<body id="page-top">
+    <section id="services" class="bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2 id=steps>Your Insurance Options</h2>
+          </div>
+        </div>
+            <p class="lead">Bronze, Silver, Gold, and Platinum Plans Information</p>
+          <div class="table" id = "options" >
+            <table width="100%">
+              <thead> 
+                <tr>
+
+                  <th style="fill: gray"> </th>
+                  <th> Benefits  </th>
+                  <th> Premium </th>
+                <th> Deductible </th>
+              </tr>
+            <div class="row">
+              <tr>
+                <th style="color: rgb(226, 226, 226)"> Platinum </th>
+              </tr>
+            </div>
+            <div class="row">
+              <tr>
+                <th style="color: rgb(255, 233, 0)"> Gold </th>
+              </tr>
+            </div>
+            <div class="row">
+              <tr>
+                <th style="color: rgb(153, 153, 153)"> Silver </th>
+              </tr>
+            </div>
+            <div class="row">
+              <tr>
+                <th style="color: rgb(193, 146, 3)"> Bronze </th>
+              </tr>
+            </div>
+            </thead>
+             
+            </table>
+            </div>
+      </div>
+    </section>
+
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2 id="steps">Our Insurance Recommendations</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+        <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom JavaScript for this theme -->
+    <script src="js/scrolling-nav.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
