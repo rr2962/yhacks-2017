ca7c69e3738b90aa3a13647eabdf0418b344eae7
chiyo <chiyo-chan@hotmail.com>
2017-12-03 03:16:44 -0500

    Fixed Chart

--- Website/action_page.php
+++ Website/action_page.php
@@ -17,11 +17,11 @@
 </head>
 
 <body id="page-top">
-    <!-- Topmost section of page: has the ability to skip down to the area of interest by click on the top right words-->
+    <!-- Topmost section of page: has the ability to skip down to the area of interest by clicking on the top right words-->
      <div class="container">
       <br>
         <!--Site Title-->
-        <a class="navbar-brand js-scroll-trigger" href="#page-top" style="font-size: 45px; color: white"> ViTech Insurance Analytics
+        <a class="navbar-brand js-scroll-trigger" href="#page-top" style="font-size: 45px"> ViTech Insurance Analytics
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -51,41 +51,69 @@
 
       <br>
     <!-- Creates the comparison table-->
-    <div class="table" id = "options" >
-      <h2>Bronze, Silver, Gold, and Platinum Plans Information</h2>
-      <table width="100%">
+      <h2>Platinum, Gold, Silver and Bronze Plans Information</h2>
+      <table width="100%" id = "options">
         <thead> 
-          <tr id="steps">
-            <th> Plan Type </th>                  
-            <th> Base Price  </th>
-            <th> AD&D <img title="Accidental Death and Dismemberment" src="http://www.free-icons-download.net/images/blue-info-button-icon-50693.png" alt="information" height="20" width="20">  </th> 
-            <th> Deductible </th>
-            <th> Coverage Amount </th>
+          <tr id= "steps">
+            <td> Plan Type </td>                  
+            <td> Base Price  </td>
+            <td> AD&D <img title="Accidental Death and Dismemberment" src="http://www.free-icons-download.net/images/blue-info-button-icon-50693.png" alt="information" height="20" width="20">  </td> 
+            <td> Deductible </td>
+            <td> Coverage Amount </td>
           </tr>
         </thead>
-      <!-- Each row creates the section for each plan.-->
-      <div class="row">
+        <tbody>
+          <div class="row">
+           <div class="col-md-3"></div>
+           <div class="col-md-3"></div>
+           <div class="col-md-3"></div>
+           <div class="col-md-3"></div>
+         </div>
+           <!--plat-->
+          <div class="row">
         <tr>
-           <th> <img src="https://blueastral.com/wp-content/uploads/platinum_opt.png" alt="Platinum" style="color: rgb(226, 226, 226)" height="120"; width="120"> </th>
+            <td> <img src="https://blueastral.com/wp-content/uploads/platinum_opt.png" alt="Platinum" style="color: rgb(226, 226, 226)" height="120"; width="120"> </td>
+            <td>110</td>
+            <td>50,000</td>
+            <td>3,000</td>
+            <td>100,000</td>
         </tr>
       </div>
+      <!--gold-->
       <div class="row">
         <tr>
-          <th> <img src="http://vignette2.wikia.nocookie.net/criminal-case-grimsborough/images/a/a9/Gold_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140102" alt="Gold" style="color: rgb(255, 233, 0)" height="140"; width="100"> </th>
+          <td> <img src="http://vignette2.wikia.nocookie.net/criminal-case-grimsborough/images/a/a9/Gold_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140102" alt="Gold" style="color: rgb(255, 233, 0)" height="140"; width="100"> </td>
+            <td>70</td>
+            <td>30,000</td>
+            <td>5,000</td>
+            <td>100,000</td>
         </tr>
       </div>
+      <!--silver-->
       <div class="row">
         <tr>
-          <th> <img src="https://vignette.wikia.nocookie.net/criminal-case-grimsborough/images/7/7f/Silver_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140409" alt="Silver"  style="color: rgb(153, 153, 153)" height="140"; width="100"> </th>
+          <td> <img src="https://vignette.wikia.nocookie.net/criminal-case-grimsborough/images/7/7f/Silver_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140409" alt="Silver"  style="color: rgb(153, 153, 153)" height="140"; width="100"> </td>
+                    
+            <td>40</td>
+            <td>20,000</td>
+            <td>7,000</td>
+            <td>100,000</td>
         </tr>
       </div>
+      <!--bronze-->
       <div class="row">
         <tr>
-          <th> <img src="http://vignette4.wikia.nocookie.net/criminal-case-grimsborough/images/7/71/Bronze_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140520" alt="Bronze" style="color: rgb(193, 146, 3);" height="140" width="100"> </th>
+          <td> <img src="http://vignette4.wikia.nocookie.net/criminal-case-grimsborough/images/7/71/Bronze_Medal_%28CCW_Awards%29.png/revision/latest?cb=20160101140520" alt="Bronze" style="color: rgb(193, 146, 3);" height="140" width="100"> </td>         
+            <td>20</td>
+            <td>10,000</td>
+            <td>8,000</td>
+            <td>100,000</td>
         </tr>
       </div>
+    </tbody>
       </table>
-    </div>
+    </div>         
+      
     <!-- Directs the user to the recommended plan-->
     <section id="recommendations">
       <div class="container">
